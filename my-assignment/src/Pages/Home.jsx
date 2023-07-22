2c16a359bdd01fe326fbde5961c00cddddd9a355
Ayush Verma <ayush98277@gmail.com>
2023-07-22 16:03:33 +0530

    both navbars and overview secsion done

---
+++ my-assignment/src/Pages/Home.jsx
@@ -0,0 +1,24 @@
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
+import SideNav from "../Components/SideNav";
+import Navbar from "../Components/Navbar";
+import Overview from "../Components/Overview";
+
+const Home = () => {
+  return (
+    <Flex justifyContent={"space-between"} pr="20px" h="200vh">
+      <SideNav />
+
+      <Box
+        w={{ base: "83%", md: "83%", lg: "88%" }}
+        position="absolute"
+        left={{ base: "16%", md: "16%", lg: "11%" }}
+      >
+        <Navbar />
+        <Overview />
+      </Box>
+    </Flex>
+  );
+};
+
+export default Home;
